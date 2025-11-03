d95d3da9dff8b720951c6f087350168303f5d0e4
smh389-gtown <smh389@georgetown.edu>
2025-11-03 11:40:36 -0500

    RPS Game
    * New Files
    
    * Winner Determination

--- README.md
+++ README.md
@@ -3,15 +3,28 @@
 Learning and Practicing Version Control
 
 
+## Setup
+Clone the Repo to download it from GitHub. Perhaps onto the Desktop.
+
+Navigate to the repo using the command line.
+
+```sh
+cd ~/Desktop/my-first-repo-fall-2025
+```
 
 
+## Usage
+Example script:
 
-This is the README.md file. It uses the markdown language.
+```sh
+python apps/my_script.py
 
-Here is a list:
+```
+Game of rock, paper, scissors:
 
-  + Item 1
-  + Item 2
-  + Item 3
+```sh
+python apps/rps.py
 
-For more information about Markdown syntax, see the [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/).
+# alternative "modular style" command
+python -m apps/rps
+```
--- apps/rps.py
+++ apps/rps.py
@@ -1,67 +1,52 @@
 import random
 
-# INITIALIZE THE STATS
-ties = 0
-wins = 0
-losses = 0
-games = 0
-keep_playing = True
-print(f'Hello Steve! Welcome to Rock, Paper, Scissors!')
-print("-------------------------------------------------------")
-while keep_playing:
-# USER INPUT
-    while True:
-        choices = ['ROCK', 'PAPER', 'SCISSORS']
-        user_choice = input(f'Please choose an option {choices}:').upper()
-    # VALIDATE THE INPUT
-        if user_choice in choices:
-            break
-        else:
-            print('Please select a valid option.')
 
-# COMPUTER SELECTION
-    computer_choice = random.choice(choices)
+VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-# SHOW SELECTIONS
-    print(f'You chose {user_choice} and the computer chose {computer_choice}')
+# ASK USER FOR AN INPUT (R/P/S)
+
+user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+print("USER:", user_choice)
+
+# VALIDATIONS
+
+if user_choice not in VALID_OPTIONS:
+    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+    # exit()
+    quit()
+
+# GENERATE RANDOM COMPUTER CHOICE
+
+computer_choice = random.choice(VALID_OPTIONS)
+print("COMP:", computer_choice)
+
+# DETERMINE THE WINNER
+
+# quick alias to facilitate some copy and pasting
+# we will soon move this into a function anyway
+u = user_choice
+c = computer_choice
+
+if u == "rock" and c == "rock":
+    print("TIE GAME")
+elif u == "rock" and c == "paper":
+    print("COMPUTER WINS")
+elif u == "rock" and c == "scissors":
+    print("USER WINS")
+elif u == "paper" and c == "rock":
+    print("COMPUTER WINS") # OOPS
+elif u == "paper" and c == "paper":
+    print("TIE GAME")
+elif u == "paper" and c == "scissors":
+    print("USER WINS") # OOPS
+elif u == "scissors" and c == "rock":
+    print("COMPUTER WINS")
+elif u == "scissors" and c == "paper":
+    print("USER WINS")
+elif u == "scissors" and c == "scissors":
+    print("TIE GAME")
 
 
-# WINNER DETERMINATION
-
-    if user_choice ==  computer_choice:
-        print('The game ended in a tie.')
-        print("-----------------------------------------------------------------")
-        ties += 1
-        games += 1
-    elif (user_choice == 'ROCK' and computer_choice == 'SCISSORS') or (user_choice == 'PAPER' and computer_choice == 'ROCK') or (user_choice == 'SCISSORS' and computer_choice == 'PAPER'):
-        print('You won!')
-        print("-----------------------------------------------------------------")
-        wins += 1
-        games += 1
-    else:
-        print('You lost.')
-        print("-----------------------------------------------------------------")
-        losses += 1
-        games += 1
 
 
-    # KEEP PLAYING
-
-    while True:
-        answer = input("Would you like to play again? (yes/no)").upper()
-        if answer == 'NO':
-            keep_playing = False
-            break
-        elif answer == 'YES':
-            break
-        else:
-            print("Please enter yes or no!")
-
-    # STATS
-    if games >0:
-        win_percentage = round((wins/games)*100,2)
-        print(f'Wins: {wins} / Losses: {losses} / Ties: {ties}')
-        print(f'Win Percentage: {win_percentage} %')
-        print("Thanks for playing!")
-        print("--------------------------------------")
 
