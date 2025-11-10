2c241b011e4ab8b3ce2ca3fc4afdfb5994c70859
smh389-gtown <smh389@georgetown.edu>
2025-11-09 20:32:18 -0500

    refractor data fetching logic

--- apps/stocks.py
+++ apps/stocks.py
@@ -8,20 +8,26 @@ load_dotenv() # loads environment variables from the ".env" file
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
-# Fetch the data
 
+def fetch_stocks_csv(symbol= "NFLX"):
+    # Fetch the data
+    symbol = "NFLX"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+    stocks_df = read_csv(request_url)
+    return stocks_df
 
+if __name__ == "main":
 
-symbol = "NFLX"
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
-
-stocks_df = read_csv(request_url)
-print(stocks_df.head())
-
-# Chart the data
-fig = px.line(stocks_df, x="timestamp", y="adjusted_close",
-              title=f"Stock Prices ({symbol})",
-              height=450
-              )
-fig.show()
+    # Fetch data
+    symbol = input("Please select a stock:") or "NFLX"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+    stocks_df = fetch_stocks_csv(symbol)
+    print(stocks_df.head())
+    
+    # Chart the data
+    fig = px.line(stocks_df, x="timestamp", y="adjusted_close",
+                title=f"Stock Prices ({symbol})",
+                height=450
+                )
+    fig.show()
 
